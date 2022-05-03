299778bd2f05705b654cec5020a12707083fb858
Ivan <alu0101388786@ull.edu.es>
2022-05-03 09:27:35 +0000

    Auto

---
+++ borrar.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    /*
+    ver mis peticiones -> identificarse -> registrarse
+                        -> editar una peticion
+    firmar una peticion -> pedir nombre apellido
+                        //-> compartir peticion
+                        //-> donar a la peticion 
+    crear una peticion   -> identificarse -> registrarse
+    //programa de socios
+    */
+
+   std::cout << " oooo  oo      oo      oo       oo      oo  |" << std::endl;
+   std::cout << "  oo    oo    oo     oo  oo     oo oo   oo  |" << std::endl;
+   std::cout << "  oo     oo  oo    oo oooo oo   oo   oo oo   " << std::endl;
+   std::cout << " oooo      oo     oo        oo  oo      oo  o"  << std::endl;
+
+    return 0;
+}
