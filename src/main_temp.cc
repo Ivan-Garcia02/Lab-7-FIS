b8c1369876d18569c3f631a34d95ef93f5d9e73f
usuario <alu0101423944@ull.edu.es>
2022-05-03 16:44:50 +0000

    aa

---
+++ src/main_temp.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <unistd.h>
+
+void Info_Dorf();
+void Register_Login();
+
+int main() {
+
+  //Info_Dorf();
+  Register_Login();
+
+
+  return 0;
+}
+
+void Info_Dorf() {
+  std::string texto;
+  std::string linea;
+  std::string intro_file{"../info_intro.txt"};
+  std::ifstream archivo(intro_file);
+
+  while (getline(archivo, linea)) {
+    texto = texto + linea + "\n";
+  }
+  archivo.close();
+
+  for (auto i : texto) {
+      std::cout << i;
+  }
+  sleep(5);
+  system("clear");
+}
+
+void Register_Login() {
+  int opc{0};
+  std::cout << "\t" << "Registrar (1):" << "\n" << "\t" << "Iniciar Sesion (2):" << std::endl;
+  std::cout << "Elija una opcion: ";
+  std::cin >> opc;
+
+}
---
+++ src/system_temp.cc
@@ -0,0 +1,13 @@
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+class System {
+ public:
+  bool CreateUser(void);
+  
+
+ private:
+  
+};
+
+#endif
