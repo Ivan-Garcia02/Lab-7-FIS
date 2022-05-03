67c593e9f1c23d77a163be8b0f37c02118ebde5d
usuario <alu0101423944@ull.edu.es>
2022-05-03 16:48:35 +0000

    aa

--- src/system_temp.cc
+++ src/system_temp.cc
@@ -1,13 +1,19 @@
-#ifndef SYSTEM_H
-#define SYSTEM_H
+#include <iostream>
+#include <string>
+#include "user.h"
+#include "system_temp.h"
 
-class System {
- public:
-  bool CreateUser(void);
-  
 
- private:
-  
-};
 
-#endif
+void System::Login(void) {
+  std::string username, password;
+  if (UserExist(GetUsername())) {
+    std::cout << "Introduzca el nombre de usuario: ";
+    std::cin >> username;
+    std::cout << "Introduzca la contraseña: ";
+    std::cin >> password;
+    if (CheckPassword(password)) {
+      
+    }
+  }
+}
