59a03536cea6318ffb1d440736cf27b04e0ec745
ramirodifonti <alu0101425030@ull.edu.es>
2022-05-03 16:07:01 +0100

    a

--- src/user.h
+++ src/user.h
@@ -5,9 +5,10 @@
 #include <vector>
 
 class User {
+ public:
   User(std::string username, std::string password, std::string name, std::string email, std::vector<bool> petitions)
        : username_{username}, password_{password}, name_{name}, email_{email}, petitions_{petitions} {}
-
+  void SetPetitions(std::vector<bool> petitions) {petitions_ = petitions; return;}
  private:
   std::string username_;
   std::string password_;
